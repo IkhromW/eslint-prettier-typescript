2ec2b960febee20a907b52fd7226ea30e984ae4f
Ikhrom Wicaksono <ikhromwicaksono@its-MacBook-Air-Ikhrom.local>
2023-04-27 14:58:39 +0700

    ignore cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,31 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended",
 		"plugin:@typescript-eslint/recommended-requiring-type-checking",
 	],
+	overrides: [
+		{
+			files: "**/*.vue",
+			parser: "@vue-eslint/parser",
+		},
+		{
+			files: "**/*.test.*",
+			rules: {
+				"@typescript-eslint/no-unsafe-call": "off",
+			},
+		},
+		{
+			extends: [
+				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+			],
+			files: "**/*.ts*",
+			parserOptions: {
+				project: ["./tsconfig.json"],
+				tsconfigRootDir: __dirname,
+			},
+		},
+	],
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
 	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		project: ["./tsconfig.json"],
-		tsconfigRootDir: __dirname,
-	},
+
 	root: true,
 };
