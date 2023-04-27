678154d0af0b28080ebbef6e94166e3de2eb8e09
Ikhrom Wicaksono <ikhromwicaksono@its-MacBook-Air-Ikhrom.local>
2023-04-27 14:49:27 +0700

    linting n extensions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,15 @@
-/* eslint-env node */
 module.exports = {
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+	],
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: ["./tsconfig.json"],
+		tsconfigRootDir: __dirname,
+	},
 	root: true,
 };
